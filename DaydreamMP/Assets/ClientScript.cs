b4182acd91cc718a7e1c8551cf1c45b3588292ef
Rex Soriano <rex@rex-soriano.com>
2018-03-13 21:23:24 -0400

    add client

---
+++ DaydreamMP/Assets/ClientScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyWiFi.ClientControls;
+using EasyWiFi.Core;
+
+public class ClientScript : MonoBehaviour {
+    public FloatDataClientController clientPosX;
+    public FloatDataClientController clientPosZ;
+    public Transform player;
+    public Transform player2;
+    // Use this for initialization
+    void Start () {
+        player2 = Instantiate(player).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        clientPosX.setValue(0);
+        clientPosZ.setValue(0);
+    }
+
+    public void UpdateServerPosX(FloatBackchannelType data )
+    {
+        Debug.Log(data.FLOAT_VALUE);
+        player2.transform.position = new Vector3(data.FLOAT_VALUE, 
+                                                player2.transform.position.y, 
+                                                player2.transform.position.z);
+
+
+    }
+
+    public void UpdateServerPosZ(FloatBackchannelType data)
+    {
+        player2.transform.position = new Vector3(player2.transform.position.x,
+                                                player2.transform.position.y,
+                                                data.FLOAT_VALUE);
+    }
+}
